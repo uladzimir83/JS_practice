c2d86dd4aad3141a6017bf9645ee2b03cf0d58bb
Uladzimir Abramkin <abramkin@scand.com>
2021-04-22 18:41:34 +0300

    Added layout for login

---
+++ src/js/modules/popup.js
@@ -0,0 +1,8 @@
+export let popup = function(options) {
+    function createWindow() {
+        let wrapper = document.createElement('div');
+
+        wrapper.classList.add('popup__wrapper');
+        wrapper.insertAdjacentHTML('afterbegin', ``)
+    }
+}
