0fe9a08896f64020d8fe5a1744cce6a3a150373b
Uladzimir Abramkin <abramkin@scand.com>
2021-04-23 17:21:56 +0300

    Added script for popup

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,8 @@
 import '../scss/main.scss';
 import '../index.html';
 
+import {popup} from './modules/popup'
+
+window.mod = popup();
+
+
--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -1,8 +1,39 @@
 export let popup = function(options) {
-    function createWindow() {
+    function createWindow(options) {
         let wrapper = document.createElement('div');
 
         wrapper.classList.add('popup__wrapper');
-        wrapper.insertAdjacentHTML('afterbegin', ``)
+        wrapper.insertAdjacentHTML('afterbegin', `
+        <div class="popup__overlay">
+            <div class="popup__body">
+                <div class="popup__header">
+                    <h3 class="popup__title">${options.title || 'Welcome'}</h3>
+                </div>
+                <div class="popup__content">
+                    <p class="popup__text">${options.text || 'Здесь могла бы быть ваша реклама'}</p>
+                </div>
+                <div class="popup__footer">
+                    <button class="popup__btn"></button>
+                    <button class="popup__btn"></button>
+                </div>
+            </div>
+        </div>
+        `)
+        document.body.prepend(wrapper);
+        return wrapper;
+    }
+    let popupBody = createWindow({title: 'Заголовок'});
+
+    return {
+        open() {
+            popupBody.classList.add('open');
+        },
+        close() {
+            popupBody.classList.remove('open');
+            popupBody.classList.add('hidden');
+            setTimeout(() => {
+                popupBody.classList.remove('hidden');
+            }, 200);
+        }
     }
 }
