f76eac281b43a61bad0eccbb5779a71f9c34296d
Uladzimir Abramkin <abramkin@scand.com>
2021-04-30 18:23:43 +0300

    Added styles for selected card

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -1,7 +1,50 @@
 export let initFilter = function() {
+    let options = {};
+
+    // open select list
     document.addEventListener('click', (e) => {
         if (e.target.dataset.screen) {
-            e.target.parentNode.classList.add('is-open');
+            closeBody(e.target);
+        }
+    });
+
+    // click on option item
+    for (let opt of document.querySelectorAll('.select__option')) {
+        opt.addEventListener('click', function(e) {
+            addSelected(this);
+            this.closest('.select__body').classList.remove('is-open');
+        });
+    }
+
+    // add selected data to object
+    var selectedOptions = {};
+    function addSelected(ths) {
+        let type = ths.closest('.select__body').dataset.type,
+            thsText = ths.textContent;
+
+        if (type in selectedOptions == false) {
+                selectedOptions[type] = [];
+                selectedOptions[type].push(thsText);
+        } else if (type in selectedOptions == true && selectedOptions[type].indexOf(thsText, 0) < 0) {
+            selectedOptions[type].push(thsText);
+        } else if (type in selectedOptions == true && selectedOptions[type].indexOf(thsText, 0) >= 0) {
+            selectedOptions[type].splice(selectedOptions[type].indexOf(thsText, 0), 1);
+        }
+    }
+
+    function closeBody(ths) {
+        for (let body of document.querySelectorAll('.select__body')) {
+            body.classList.remove('is-open');
+            if(ths) {
+                ths.closest('.select__body').classList.add('is-open');
+            }
+        }
+    }
+
+     // close select body after click out of the body
+    window.addEventListener('click', function(e) {
+        if (!e.target.closest('.select__body')) {
+        closeBody();
         }
     });
 }
