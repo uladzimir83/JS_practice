a6f49f2fb67c75ee31bb52458b84adbd250be165
Uladzimir Abramkin <abramkin@scand.com>
2021-05-03 20:01:28 +0300

    Changed styles for filter and added multiselect options

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -10,26 +10,51 @@ export let initFilter = function() {
 
     // click on option item
     for (let opt of document.querySelectorAll('.select__option')) {
+        let parentOpt = opt.closest('.select__body');
         opt.addEventListener('click', function(e) {
-            addSelected(this);
-            this.closest('.select__body').classList.remove('is-open');
+            parentOpt.classList.contains('is-multiple') ? addMultiSelect(this) : addSingleSelect(this);  
         });
     }
 
     // add selected data to object
-    var selectedOptions = {};
-    function addSelected(ths) {
-        let type = ths.closest('.select__body').dataset.type,
-            thsText = ths.textContent;
+    var optionSelected = {};
 
-        if (type in selectedOptions == false) {
-                selectedOptions[type] = [];
-                selectedOptions[type].push(thsText);
-        } else if (type in selectedOptions == true && selectedOptions[type].indexOf(thsText, 0) < 0) {
-            selectedOptions[type].push(thsText);
-        } else if (type in selectedOptions == true && selectedOptions[type].indexOf(thsText, 0) >= 0) {
-            selectedOptions[type].splice(selectedOptions[type].indexOf(thsText, 0), 1);
+    function addMultiSelect(ths) {
+        let parent = ths.closest('.select__body'),
+            type = parent.dataset.type,
+            text = ths.textContent,
+            screen = parent.querySelector('.select__screen'),
+            clearBtn = screen.querySelector('.select__clear');
+
+        if (type in optionSelected == false) {
+                optionSelected[type] = [];
+                optionSelected[type].push(text);
+        } else if (type in optionSelected == true && optionSelected[type].indexOf(text, 0) < 0) {
+            optionSelected[type].push(text);
+        } else if (type in optionSelected == true && optionSelected[type].indexOf(text, 0) >= 0) {
+            optionSelected[type].splice(optionSelected[type].indexOf(text, 0), 1);
         }
+
+        screen.innerHTML = optionSelected[type].join(', ');
+        addClearBtn(ths);
+
+        if (optionSelected[type].length == 0) {
+            delete optionSelected[type];
+            screen.innerHTML = 'Choose';
+            clearBtn.remove();
+            closeBody(null);
+        }
+    }
+
+    function addSingleSelect(ths) {
+        let parent = ths.closest('.select__body'),
+            type = parent.dataset.type,
+            screen = parent.querySelector('.select__screen');
+
+        screen.innerHTML = ths.textContent;
+        optionSelected[type] = ths.textContent;
+        addClearBtn(ths);
+        closeBody(null);
     }
 
     function closeBody(ths) {
@@ -40,6 +65,24 @@ export let initFilter = function() {
             }
         }
     }
+
+    function addClearBtn(ths) {
+        let isBtn = ths.closest('.select__body').querySelector('.select__screen'),
+            btnClear = document.createElement('span');
+
+        btnClear.classList.add('select__clear');
+
+        isBtn.querySelector('.select__clear') ? null : isBtn.append(btnClear);
+    }
+
+    document.addEventListener('click', (e) => {
+        if (e.target.parentNode.dataset.screen) {
+            let attr = e.target.closest('.select__body').dataset.type;
+            delete optionSelected[attr];
+            e.target.parentNode.innerHTML = 'Choose';
+            e.target.remove();
+        }
+    });
 
      // close select body after click out of the body
     window.addEventListener('click', function(e) {
